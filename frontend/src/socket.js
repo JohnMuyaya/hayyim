485bdc992ecc5139428a345cd07e0a16d3cc3725
JOHN MUYAYA <johnmuyaya672@gmail.com>
2026-03-20 22:36:58 +0100

    Update https://hayyim-1.onrender.com for production

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 export const connectSocket = (username, onMessage) => {
-  const socket = new WebSocket(`ws://localhost:8000/ws/${username}`);
+  const socket = new WebSocket(`wss://hayyim.onrender.com/ws/${username}`);
   socket.onopen = () => console.log('WebSocket connected');
   socket.onmessage = (e) => onMessage(JSON.parse(e.data));
   socket.onerror = (error) => console.error('WebSocket error:', error);
